11b34651bf19a887646c0f265d7d19c13fe0ab2d
Trocha4 <trocha@fi.uba.ar>
2025-05-03 19:47:01 -0300

    Delete movie y put movie

--- backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/movies/MoviesRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.uba.fi.ingsoft1.todo_template.movies;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface MoviesRepository extends JpaRepository<Movies,Long>{
-    Optional<Movies> findByTitle(String title);
-}
