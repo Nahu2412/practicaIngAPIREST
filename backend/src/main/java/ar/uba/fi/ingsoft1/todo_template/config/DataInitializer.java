3a6d8ec95011328fde3c5cd811e9fd4e3389d6ad
julian <julalvarez@fi.uba.ar>
2025-05-05 18:51:47 -0300

    edit and get user profile

--- backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/config/DataInitializer.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/config/DataInitializer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+/*
 import java.time.LocalDate;
 
 @Configuration
@@ -21,4 +21,4 @@ public class DataInitializer {
             }
         };
     }
-}
+}*/
--- backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/user/UserRestController.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/user/UserRestController.java
@@ -2,8 +2,11 @@ package ar.uba.fi.ingsoft1.todo_template.user;
 
 import ar.uba.fi.ingsoft1.todo_template.common.exception.ItemNotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,6 +37,33 @@ class UserRestController {
         return userService.createUser(data)
                 .map(tk -> ResponseEntity.status(HttpStatus.CREATED).body(tk))
                 .orElse(ResponseEntity.status(HttpStatus.CONFLICT).build());
+    }
+
+    @GetMapping(value = "/{id}/profile", produces = "application/json")
+    @Operation(summary = "Get user profile")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content)
+    @ApiResponse(responseCode = "404", description = "User not found", content = @Content)
+    public UserProfileDTO getUserProfile(
+            @Valid @PathVariable @Positive long id
+    ) throws MethodArgumentNotValidException, ItemNotFoundException {
+        return userService.getUserProfile(id);
+    }
+
+    @PutMapping("/{id}/profile")
+    @Operation(summary = "Edit user profile")
+    public ResponseEntity<UserProfileDTO> editUserProfile(
+            @PathVariable Long id,
+            @Valid @RequestBody UserProfileDTO userProfileDTO) {
+        try {
+            UserProfileDTO updatedUserProfile = userService.editUserProfile(id, userProfileDTO);
+            return ResponseEntity.ok(updatedUserProfile);
+        } catch (ItemNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(null);
+        }
     }
 
     @GetMapping(value = "/{id}/followers", produces = "application/json")
